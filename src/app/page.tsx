3b8fc588f2fdbfe0506f536b461e0a61cd6df825
danieltsega <danieltsega6658@gmail.com>
2024-11-01 21:23:47 +0300

    update: make more layout changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,54 @@
 import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { tasks } from "../lib/data";
+import Link from "next/link";
+import TaskCard from "@/components/TaskCard";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <div className="md:max-w-4xl mx-auto relative pt-24">
+        <div className="flex justify-between  gap-16">
+          <div>
+            <Card>
+              <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+          </div>
+          <div className="flex-1">
+            <Card>
+              <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
