14ccd413a6640f755d799c207556db046728e013
danieltsega <danieltsega6658@gmail.com>
2024-11-15 10:43:29 +0300

    fix: fix thumbnail image name

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -5,7 +5,7 @@ export const tasks = [
     title: "Renew Driver’s License",
     description:
       "Detailed steps to renew your driver’s license at the government office...",
-    coverImage: "/path-to-image/license-renewal.jpg",
+    coverImage: "/thumbnail.jfif",
     uploadDate: "2024-10-31",
   },
   {
@@ -13,7 +13,7 @@ export const tasks = [
     title: "Passport Application",
     description:
       "Learn how to apply for a passport with these step-by-step instructions...",
-    coverImage: "/path-to-image/passport-application.jpg",
+    coverImage: "/thumbnail.jfif",
     uploadDate: "2024-10-30",
   },
   // Add more tasks as needed
